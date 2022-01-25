e88a4a0746a22e82e0a7860f421851566b7233c1
SheepyChris <christopher.reinbrecht@gmail.com>
2022-01-25 18:47:14 -0500

    Tweak gameplay fail transition

--- BGAnimations/ScreenGameplay failed.lua
+++ BGAnimations/ScreenGameplay failed.lua
@@ -2,14 +2,20 @@ return Def.ActorFrame {
 	Def.Quad {
 		StartTransitioningCommand=function(self)
 			self:FullScreen():diffuse(Color.Black)
-			:diffusealpha(0):sleep(1.5):diffusealpha(1)
+			:diffusealpha(0):linear(1.5):diffusealpha(1)
 		end
 	},
 	
 	Def.Quad {
 		StartTransitioningCommand=function(self)
-			self:FullScreen():diffuse(Color.White)
-			:diffusealpha(0):linear(1.5):diffusealpha(1):sleep(0.016):diffusealpha(0)
+			self:zoomto(256, 224):Center():MaskSource()
+		end
+	},
+	
+	Def.Quad {
+		StartTransitioningCommand=function(self)
+			self:FullScreen():diffuse(Color.White):MaskDest()
+			:diffusealpha(0):sleep(0.1):linear(1.5):diffusealpha(1):sleep(0.001):diffusealpha(0)
 			:sleep(2.5):diffusealpha(1):linear(1.5):diffusealpha(0)
 		end
 	},
@@ -17,7 +23,8 @@ return Def.ActorFrame {
 	Def.Quad {
 		StartTransitioningCommand=function(self)
 			self:zoomto(256, 224):Center():diffuse(Color.White)
-			:diffusealpha(0):linear(1):diffusealpha(1)
+			:MaskDest():ztestmode("ZTestMode_WriteOnFail")
+			:diffusealpha(0):linear(1.5):diffusealpha(1)
 			:sleep(2.5):linear(1.5):diffusealpha(0)
 		end
 	},
